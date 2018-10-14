a2483c1d010106b9fbf8b3872ab9cbde3dd4043d
jazzrabbit <cloudkim2024@gmail.com>
2018-10-14 08:06:35 -0400

    dev/PageLoading : Basic pageloading is completed

--- webTestCases.py
+++ webTestCases.py
@@ -1,31 +1,37 @@
 import time, re
 import json
 import urllib.request
-import constants
 import pytest
+from constants import Constants
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException
 from selenium.webdriver import ActionChains as ac 
 from time import sleep
+from configparser import SafeConfigParser
 
 
 @pytest.mark.functional
 def test_GoogleHomePage_connectivity():
-	assert basic(3) ==4
+    url = "https://google.com"
+    #driver = webdriver.Firefox(executable_path=Constants.WEBDRIVER_FIREFOX_PATH)
+    driver = webdriver.Chrome(executable_path=Constants.WEBDRIVER_CHROME_PATH)
+    driver.get(url)
+
+    assert basic(3) ==4
 
 def test_GoogleHomePage_logo_image():
-	print('1')
+    print('1')
 
 def test_GoogleHomePage_text_field():
-	print('2')
+    print('2')
 
 def test_GoogleHomePage_search_button():
-	print('3')
+    print('3')
 
 
 def basic(x):
-	return x+1
+    return x+1
 
 
 if __name__ == "__main__":
-	main()
+    main()
